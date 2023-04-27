9e548e2407ce345c2827ddd16fa96f4cd989b532
gargantua892 <94129392+And18892@users.noreply.github.com>
2023-04-27 22:13:05 +0200

    inserimento data dinamica e aggiustamenti vari

--- js/main.js
+++ js/main.js
@@ -10,16 +10,25 @@ let randomSeatNumber = Math.floor(Math.random() * 9);
 let randomKm =Math.floor((Math.random() * 1000)+1);
 let totalPrice;
 
+//Data odierna
+const date = new Date();
+
+let day = date.getDate() + 3;
+let month = date.getMonth() + 1;
+let year = date.getFullYear();
+let currentDate = `${day}-${month}-${year}`;
+document.getElementById("current-date").innerHTML = currentDate;
 
 
-//mettere città di partenza
+
+//Inserimento città di partenza
 departureCity = prompt("inserisci la città di partenza");
 console.log(departureCity);
 document.getElementById("departure").innerHTML = departureCity;
 
 console.log(randomKm);
 
-//Città di arrivo
+//Inserimento Città di arrivo
 arrivalCity = prompt("inserisci la città di arrivo");
 console.log(arrivalCity);
 document.getElementById("arrival").innerHTML = arrivalCity;
@@ -31,7 +40,7 @@ document.getElementById("distance").innerHTML = `${randomKm} Km`;
 const now = new Date();
 const departureHour = now.getHours() + ':' + now.getMinutes();
 console.log(departureHour);
-document.getElementById("departure-time").innerHTML = departureHour;
+document.getElementById("departure-time").innerHTML = `${departureHour}`;
 
 //posto a sedere random
 document.getElementById("seggiolino").innerHTML =  `${randomSeatNumber}A`;
@@ -45,21 +54,21 @@ console.log(globalPrice);
 
 
 //Setup sconti
-if (age < 18){
+if (age < 18){//Sconto young
     youngDiscount = (globalPrice - (globalPrice*0.20)).toFixed(2);
     document.getElementById("total-price").innerHTML =  `${youngDiscount}€`;
+    document.getElementById("discount-label").innerHTML =  "Sconto Junior 20%";
     document.getElementById("tipo-passeggero").innerHTML =  "Junior";
-    document.getElementById("discount-label").innerHTML =  "Sconto Junior";
     console.log(youngDiscount);
-} else if (age > 65){
+} else if (age > 65){//Sconto senior
     oldDiscount = (globalPrice - (globalPrice*0.40)).toFixed(2);
     document.getElementById("total-price").innerHTML =  `${oldDiscount}€`;
-    document.getElementById("discount-label").innerHTML =  "Sconto Senior";
+    document.getElementById("discount-label").innerHTML =  "Sconto Senior 40%";
     document.getElementById("tipo-passeggero").innerHTML =  "Senior";
     console.log(oldDiscount);
 }
-else{
+else{//prezzo regolare
     document.getElementById("total-price").innerHTML =  `${globalPrice}€`;
     document.getElementById("tipo-passeggero").innerHTML =  "Ordinario";
+    console.log(globalPrice);
 }
-// oldDiscount;
